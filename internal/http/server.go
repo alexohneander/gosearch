64efaf5514a8f6d609f212c36bf8e5436b607495
Alex Wellnitz <moin@wellnitz-alex.de>
2025-03-26 09:32:00 +0100

    fix: add default log level

--- internal/http/server.go
+++ internal/http/server.go
@@ -2,6 +2,7 @@ package http
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/log"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
@@ -12,6 +13,7 @@ func StartService() {
 	app.Use(logger.New(logger.Config{
 		Format: "[${ip}]:${port} ${status} - ${method} ${path}\n",
 	}))
+	log.SetLevel(log.LevelInfo)
 
 	// Configure Routes
 	app = configureRoutes(app)
